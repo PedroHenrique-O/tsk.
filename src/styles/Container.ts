3979ad9ee23fe6d035774c90ef7b29b7e8bd06b2
Pedro <pedrokaz.henrique@gmail.com>
2022-01-08 23:49:44 -0300

    global container

---
+++ src/styles/Container.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1120px;
+  height: auto;
+  margin: 0 auto;
+
+  @media screen and (max-width: 1080px) {
+    padding-right: 16px;
+    padding-left: 16px;
+  }
+`;
